8a65d58440a68c1c8e8465859a0426b327871cb1
Harsh <harsh@appmatictech.com>
2024-09-30 18:44:15 +0530

    Added some test changes

--- DemoFramework/DemoClass.swift
+++ DemoFramework/DemoClass.swift
@@ -10,7 +10,8 @@ import Foundation
 public class DemoClass {
     
     public static func enterTheNumber(num : Int){
-        
+        xscsc
+
         for i in 1...num{
             print(i)
         }
