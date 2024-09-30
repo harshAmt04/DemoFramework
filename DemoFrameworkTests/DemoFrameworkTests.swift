44955c1b94bbc0834238a2db555a14f60a3ba096
Harsh <harsh@appmatictech.com>
2024-09-30 17:12:02 +0530

    Initial Commit

---
+++ DemoFrameworkTests/DemoFrameworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoFrameworkTests.swift
+//  DemoFrameworkTests
+//
+//  Created by Harsh on 30/09/24.
+//
+
+import Testing
+@testable import DemoFramework
+
+struct DemoFrameworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
