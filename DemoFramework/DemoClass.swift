d1564dbb2817576736eec2965774f7a74d7a99e6
Harsh <harsh@appmatictech.com>
2024-09-30 18:14:41 +0530

    adding files

---
+++ DemoFramework/DemoClass.swift
@@ -0,0 +1,20 @@
+//
+//  DemoClass.swift
+//  DemoFramework
+//
+//  Created by Harsh on 30/09/24.
+//
+
+import Foundation
+
+public class DemoClass {
+    
+    public static func enterTheNumber(num : Int){
+        
+        for i in 1...num{
+            print(i)
+        }
+        
+    }
+    
+}
